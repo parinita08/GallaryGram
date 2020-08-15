c3d3acf93e148acf31830b5d79dbaa12a7eee0c2
Parinita Vedantam <vedantampari@gmail.com>
2020-08-15 14:54:14 +0530

    a-e

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>GallaryGram</h1>
+      <h1>GalleryGram</h1>
       <h2>Your Pictures</h2>
       <p>Add pictures and refresh your memories!</p>
     </div>
